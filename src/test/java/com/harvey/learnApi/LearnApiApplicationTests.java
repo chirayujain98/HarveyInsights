b70f2d5acd16a38f568b14baa8d71fbe88f62b6c
chirayujain98 <chirayu.jain98@gmail.com>
2025-04-28 11:24:58 +0530

    Initial structure

---
+++ src/test/java/com/harvey/learnApi/LearnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harvey.learnApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
